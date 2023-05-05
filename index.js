34ba8b611c7051b803170fc8edce266c46d3f1f6
RoopamTaneja <roopamtaneja999@gmail.com>
2023-05-05 21:46:03 +0530

    image to font in progress

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-function show(){
+function instishow(){
     document.getElementById("insti").style.display="block";
+}
+
+function adminshow(){
+    document.getElementById("admin").style.display="block";
+}
+
+function addshow(){
+    document.getElementById("add").style.display="block";
+}
+
+function acadshow(){
+    document.getElementById("acads").style.display="block";
+}
+
+function camshow(){
+    document.getElementById("campus").style.display="block";
+}
+
+function deptshow(){
+    document.getElementById("depts").style.display="block";
+}
+
+function careershow(){
+    document.getElementById("careers").style.display="block";
+}
+
+function plcmntshow(){
+    document.getElementById("plcmnt").style.display="block";
+}
+
+function rsshow(){
+    document.getElementById("resources").style.display="block";
 }
