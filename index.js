efb901b2cfee1b902d369ba932f10dd76ddfe7c7
RoopamTaneja <roopamtaneja999@gmail.com>
2023-05-06 12:15:01 +0530

    responsiveness

--- index.js
+++ index.js
@@ -1,35 +1,71 @@
-function instishow(){
-    document.getElementById("insti").style.display="block";
+function instishow() {
+    var lists = document.querySelectorAll('.list');
+    for (var j = 0; j < lists.length; j++) {
+        lists[j].style.display = 'none';
+    }
+    document.getElementById("insti").style.display = "block";
 }
 
-function adminshow(){
-    document.getElementById("admin").style.display="block";
+function adminshow() {
+    var lists = document.querySelectorAll('.list');
+    for (var j = 0; j < lists.length; j++) {
+        lists[j].style.display = 'none';
+    }
+    document.getElementById("admin").style.display = "block";
 }
 
-function addshow(){
-    document.getElementById("add").style.display="block";
+function addshow() {
+    var lists = document.querySelectorAll('.list');
+    for (var j = 0; j < lists.length; j++) {
+        lists[j].style.display = 'none';
+    }
+    document.getElementById("add").style.display = "block";
 }
 
-function acadshow(){
-    document.getElementById("acads").style.display="block";
+function acadshow() {
+    var lists = document.querySelectorAll('.list');
+    for (var j = 0; j < lists.length; j++) {
+        lists[j].style.display = 'none';
+    }
+    document.getElementById("acads").style.display = "block";
 }
 
-function camshow(){
-    document.getElementById("campus").style.display="block";
+function camshow() {
+    var lists = document.querySelectorAll('.list');
+    for (var j = 0; j < lists.length; j++) {
+        lists[j].style.display = 'none';
+    }
+    document.getElementById("campus").style.display = "block";
 }
 
-function deptshow(){
-    document.getElementById("depts").style.display="block";
+function deptshow() {
+    var lists = document.querySelectorAll('.list');
+    for (var j = 0; j < lists.length; j++) {
+        lists[j].style.display = 'none';
+    }
+    document.getElementById("depts").style.display = "block";
 }
 
-function careershow(){
-    document.getElementById("careers").style.display="block";
+function careershow() {
+    var lists = document.querySelectorAll('.list');
+    for (var j = 0; j < lists.length; j++) {
+        lists[j].style.display = 'none';
+    }
+    document.getElementById("careers").style.display = "block";
 }
 
-function plcmntshow(){
-    document.getElementById("plcmnt").style.display="block";
+function plcmntshow() {
+    var lists = document.querySelectorAll('.list');
+    for (var j = 0; j < lists.length; j++) {
+        lists[j].style.display = 'none';
+    }
+    document.getElementById("plcmnt").style.display = "block";
 }
 
-function rsshow(){
-    document.getElementById("resources").style.display="block";
+function rsshow() {
+    var lists = document.querySelectorAll('.list');
+    for (var j = 0; j < lists.length; j++) {
+        lists[j].style.display = 'none';
+    }
+    document.getElementById("resources").style.display = "block";
 }
